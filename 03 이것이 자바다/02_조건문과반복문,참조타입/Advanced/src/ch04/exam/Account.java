b5d4f7ab2646bcf7075da3f1c31666ae3d77a894
yurissssss <yur1n9@kakao.com>
2025-04-20 01:35:48 +0900

    ✏️ Update  : 주석 추가 04-19

--- 03 이것이 자바다/02_조건문과반복문,참조타입/Advanced/src/ch04/exam/Account.java
+++ 03 이것이 자바다/02_조건문과반복문,참조타입/Advanced/src/ch04/exam/Account.java
@@ -7,9 +7,10 @@ import java.util.Scanner;
 public class Account {
     public static void main(String[] args) {
         boolean run = true;
-        int balance = 0;
+        int balance = 0;  //잔고
         Scanner scanner = new Scanner(System.in);
 
+        //무한루프
         while (run) {
             System.out.println("-----------------------------");
             System.out.println("1.예금 | 2.출금 | 3.잔고 | 4.종료");
@@ -39,7 +40,7 @@ public class Account {
                 }
                 //종료
                 case 4 -> {
-                    run = false;
+                    run = false;  //무한루프 종료
                     System.out.println("프로그램 종료");
                     break;
                 }
