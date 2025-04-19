907145fc4282d819dedb84b336615adb4544fb54
yurissssss <yur1n9@kakao.com>
2025-04-20 01:53:02 +0900

    ✏️ Update : 주석 수정 04-19

--- 03 이것이 자바다/02_조건문과반복문,참조타입/Advanced/src/ch04/exam/Triangle.java
+++ 03 이것이 자바다/02_조건문과반복문,참조타입/Advanced/src/ch04/exam/Triangle.java
@@ -2,9 +2,9 @@ package ch04.exam;
 
 public class Triangle {
     public static void main(String[] args) {
-        //열
+        //행
         for (int i=1; i<=5; i++) {
-            //행
+            //열
             for (int j=1; j<=i; j++) {
                 System.out.print("*");
             }
--- 03 이것이 자바다/02_조건문과반복문,참조타입/Advanced/src/ch04/exam/TriangleReverse.java
+++ 03 이것이 자바다/02_조건문과반복문,참조타입/Advanced/src/ch04/exam/TriangleReverse.java
@@ -2,9 +2,9 @@ package ch04.exam;
 
 public class TriangleReverse {
     public static void main(String[] args) {
-        //열
+        //행
         for (int i=5; i>0; i--) {
-            //행
+            //열
             for (int j=1; j<=i; j++) {
                 System.out.print("*");
             }
