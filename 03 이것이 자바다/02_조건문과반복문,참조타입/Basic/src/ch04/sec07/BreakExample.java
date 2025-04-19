bd4eaabdc4bea56e7d2131fb17832ad399fe60f3
yurissssss <yur1n9@kakao.com>
2025-04-19 23:23:06 +0900

    📁 add  : BreakExample.java 04-19

--- 03 이것이 자바다/02_조건문과반복문,참조타입/Basic/src/ch04/sec07/BreakExample.java
+++ 03 이것이 자바다/02_조건문과반복문,참조타입/Basic/src/ch04/sec07/BreakExample.java
@@ -5,6 +5,7 @@ public class BreakExample {
         while (true) {
             int num = (int)(Math.random()*6) +1;  //1-6
             System.out.println(num);
+            //6이 나오면 프로그램 종료
             if (num == 6) {
                 System.out.println("프로그램 종료");
                 break;
