0dad64b7b83835083667a0ab04183f48bfdc04c6
yurissssss <yur1n9@kakao.com>
2025-04-20 01:33:34 +0900

    📁 add  : Account.java 04-19

---
+++ 03 이것이 자바다/02_조건문과반복문,참조타입/Advanced/src/ch04/exam/Account.java
@@ -0,0 +1,50 @@
+package ch04.exam;
+
+import com.sun.security.jgss.GSSUtil;
+
+import java.util.Scanner;
+
+public class Account {
+    public static void main(String[] args) {
+        boolean run = true;
+        int balance = 0;
+        Scanner scanner = new Scanner(System.in);
+
+        while (run) {
+            System.out.println("-----------------------------");
+            System.out.println("1.예금 | 2.출금 | 3.잔고 | 4.종료");
+            System.out.println("-----------------------------");
+            System.out.print("선택> ");
+
+            int menuNum = Integer.parseInt(scanner.nextLine());
+
+            switch (menuNum) {
+                //예금
+                case 1 -> {
+                    System.out.print("예금액> ");
+                    balance += Integer.parseInt(scanner.nextLine());
+                    break;
+                }
+                //출금
+                case 2 -> {
+                    System.out.print("출금액> ");
+                    balance -= Integer.parseInt(scanner.nextLine());
+                    break;
+                }
+                //잔고
+                case 3 -> {
+                    System.out.print("잔고> ");
+                    System.out.println(balance);
+                    break;
+                }
+                //종료
+                case 4 -> {
+                    run = false;
+                    System.out.println("프로그램 종료");
+                    break;
+                }
+            }
+            System.out.println();
+        }
+    }
+}
